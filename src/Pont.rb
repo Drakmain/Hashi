60a991baabc40631900a59ac5890bc10f37ce564
Boucharinc Billy <s191498@ic2s122-15.univ-lemans.fr>
2022-02-25 10:09:24 +0100

    Billy Pont

--- src/Pont.rb
+++ src/Pont.rb
@@ -1,11 +1,30 @@
 class Pont extend Element
 
-    
+    # La classe Pont possède une variable d'instance
+	#
+	# @sensHorizontal = un bool représentant le sens du pont, horizontal si true, et vertical si false
 
 
+	def initialize()
+		@sensHorizontal = true
+	end
 
 
+    # Créer les getters et setters de la variable sensHorizontal
+
+    attr_accessor :sensHorizontal
 
 
+    # Méthode qui retourne vrai
+
+    def estPont?
+        return true
+    end
+
+    # Méthode qui retourne faux
+
+    def estIle?
+        return false
+    end
 
 end

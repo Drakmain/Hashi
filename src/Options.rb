26018657f0c5327bbccddbd46df8b1963f36d345
Haran Andy <s191710@ic2s112-02.univ-lemans.fr>
2022-03-14 18:06:13 +0100

    Ajout des options

--- src/Options.rb
+++ src/Options.rb
@@ -9,22 +9,48 @@ class Options
 	##
 	# Methode qui charge les donnees de 
 	def initialize()
-		fichierOptions = File.read('../data/settings/settings.json')
-		hashOptions=JSON.parse(file)
+		@fichierOptions = File.read('../data/settings/settings.json')
+		@hashOptions=JSON.parse(@fichierOptions)
+		@user=@hashOptions["username"]
+		@ratio = @hashOptions["resolutionratio"]
+		@theme = @hashOptions["theme"]
+		@langue = @hashOptions["langue"]
 	end
 
 	def enregistrer()
-		fichierOptions.write('../data/settings/settings.json',JSON.dump(hashOptions)
+		fichier=File.open('../data/settings/settings.json','w')
+		fichier.write(JSON.dump(@hashOptions))
+	end
 	# update le nom d'utilisateur
 	def updateUser(user)
-		hashOptions["username"]=user
+		@hashOptions["username"]=user
+		@user=user
+	end
 	# update le ratio par le quelle on multiplie le format de l'ecran. Exemple 16/9 * 120 = 1920*1080. 100 donne 1600*900, 80 donne 1280*720
 	def updateResolutionratio(ratio)
-		hashOptions["resolutionratio"]=ratio
+		@hashOptions["resolutionratio"]=ratio
+		@ratio=ratio
+	end
 	# update le theme, normalement claire ou sombre
 	def updateTheme(theme)
-		hashOptions["theme"]=theme
-	# update la langue, normalement fr,es ou en
+		@hashOptions["theme"]=theme
+		@theme=theme
+	end
+	# update la langue, normalement fr, es ou en
 	def updateLangue(langue)
-		hashOptions["langue"]=langue
+		@hashOptions["langue"]=langue
+		@langue=langue
+	end
+		
+	def to_s()
+		puts("Username : #{@user} \nResolution : #{(9*@ratio.to_i())}p \nLangue : #{@langue} \nTheme : #{@theme}\n")
+	end
 end
+
+option=Options.new()
+option.updateTheme("sombre")
+option.enregistrer()
+print(option)
+option.updateTheme("claire")
+option.enregistrer()
+print(option)

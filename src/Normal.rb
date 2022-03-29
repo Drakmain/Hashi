584df8c33a25c1fa43a13c99d802f2c3e7d93b36
pgarcon <pierre.garcon@outlook.fr>
2022-03-29 09:56:07 +0200

    commentaire

--- src/Normal.rb
+++ src/Normal.rb
@@ -1,15 +1,34 @@
+load "Chrono.rb"
+
+
+# class Normal
+#
+# La classe normal est une sous-classe de la classe contre-la-montre, elle possède les mêmes caractèristiques
+# la seule chose qui change est le sens du chronometre
+#
+#  Elle peut donc faire tous ce que peut faire la classe ContreLaMontre
+#
 class Normal < ContrelaMontre
 
+    # New est privée
     private_class_method :new
 
-    def Normal.creer(unFichier)
-        super(unFichier)
+    # Normal.creer
+    #
+    # permet de creer un nouveau mode normal
+    #
+    # ==== ATTRIBUTS
+    # 
+    # *+unPlateau+ : unPlateau
+    #
+    #
+    def Normal.creer(unPlateau, unNiveau, unPseudo, uneDifficulte)
+        super(unPlateau, unNiveau, unPseudo, uneDifficulte)
     end
 
-    #************************************
-    #           lancerChrono()
+    #   lancerChrono
     #
-    #permet de lancer le chronometre dans le sens normal (part de 0 et s'incrémente jusqu'à ce que la partie soit terminé)
+    # permet de lancer le chronometre dans le sens normal (part de 0 et s'incrémente jusqu'à ce que la partie soit terminé)
     def lancerChrono()
         @chrono.lancerChrono
     end

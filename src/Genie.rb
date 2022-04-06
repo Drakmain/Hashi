18895152a472fd5d1e6161a1564a59086484d23d
pgarcon <pierre.garcon@outlook.fr>
2022-04-06 14:35:46 +0200

    classement

--- src/Genie.rb
+++ src/Genie.rb
@@ -433,7 +433,7 @@ class Genie
   # L'état de la partie en cours
   #
   def to_s
-    "Je suis en mode génie \nplateau : #{@plateau}\nScore : #{@score}"
+    "genie"
   end
 
   # Méthode qui affiche le plateau sur le terminal
@@ -459,7 +459,7 @@ class Genie
 
 
   def sauvegarder_score()
-    fichierScore = "../data/map/#{@difficulte.to_s}/score/#{@niveau.to_s}.txt"
+    fichierScore = "../data/map/#{@difficulte.to_s}/score/#{@niveau.to_s}#{self.to_s}.txt"
     open(fichierScore, 'a'){
       |f| f.puts @pseudo + "_" + @score.to_s() + "_" + @chrono.chrono.to_s
     }
